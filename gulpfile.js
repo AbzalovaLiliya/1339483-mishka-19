881c34075b31ba92e0ace0a0fbca6c5408d26f12
AbzalovaLiliya <42464223+AbzalovaLiliya@users.noreply.github.com>
2020-05-22 23:38:58 +0300

    исправление ошибок

--- gulpfile.js
+++ gulpfile.js
@@ -25,11 +25,12 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(gulp.dest("build/css"))
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("build/css"))
-  /*.pipe(server.stream());*/
+    .pipe(server.stream());
 });
 
 gulp.task("images", function () {
