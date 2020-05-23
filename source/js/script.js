6864150b3b79c13e04cdad1f152224ecb8898167
AbzalovaLiliya <42464223+AbzalovaLiliya@users.noreply.github.com>
2020-05-23 12:59:08 +0300

    исправление ошибок

--- source/js/script.js
+++ source/js/script.js
@@ -3,12 +3,13 @@
 const pageHeader = document.querySelector(".page-header");
 // без включенного js меню будет показано по умолчанию
 pageHeader.classList.add("page-header--closed");
+pageHeader.classList.remove("page-header--nojs");
+
 const buttonToggle = document.querySelector(".page-header__toggle");
 buttonToggle.addEventListener("click", function (evt) {
   evt.preventDefault();
   buttonToggle.classList.toggle("page-header__toggle--active");
   console.log("pageHeader", pageHeader);
-
   pageHeader.classList.toggle("page-header--closed");
 });
 
